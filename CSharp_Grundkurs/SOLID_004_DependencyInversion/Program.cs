30b2a141b736310f63a717bcc094447fe309fdbe
Kevin-Winter-Codes <Kevin.Winter.Privat@gmail.com>
2022-02-10 15:18:46 +0100

    SOLID fertig

--- CSharp_Grundkurs/SOLID_004_DependencyInversion/Program.cs
+++ CSharp_Grundkurs/SOLID_004_DependencyInversion/Program.cs
@@ -6,7 +6,108 @@ namespace SOLID_004_DependencyInversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICar car = new Car() { Brand = "VW", Model = "Polo", ConstructionYear = 2021 };
+
+            //Für Testzwecke:
+            ICar mockCar = new MockCar(); 
+
+            ICarService service = new CarService();
+            service.Repair(mockCar); //zum Testen
+            service.Repair(car); //Produktiv 
+
         }
     }
+
+    #region Bad Sample
+
+
+    //Programmier A: 5 Tage -> Tage 1 bis 5 am Arbeiten
+    //Beispiel 2-> BadCar -> Entities.dll
+    public class BadCar
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ConstructionYear { get; set;  }
+    }
+
+
+    //Programmierer B: 3 Tage -> 5 oder 6 -> arbeitet bis mindesten Tag 8
+    //Beispiel 2-> BadCarService -> Service.dll
+    public class BadCarService
+    {
+
+        //Feste Kopplung 
+        public void Repair(BadCar car)
+        {
+            //repariere Auto 
+        }
+    }
+
+    #endregion
+
+
+    #region Interfaces bringen eine lose Kopplung
+    //Contract First -> Wir schreiben zuerst einmal die Interfaces 
+
+
+
+    
+
+
+    #region Entities.dll
+    //Programmierer: 5 Tage (Tag 1 bis 5) 
+    public class Car : ICar
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int ConstructionYear { get; set; }
+
+        public void StarteMotor()
+        {
+            //Neue Aufgabe
+        }
+    }
+    #endregion
+
+
+    #region Interface.dll
+    //Dauer ein Meeting
+    //Programmierer A:
+    public interface ICar
+    {
+        string Brand { get; set; }
+        string Model { get; set; }
+        int ConstructionYear { get; set; }
+    }
+
+    //Programmierer B
+    public interface ICarService
+    {
+        void Repair(ICar car);
+    }
+    #endregion
+
+    #region CarService.dll
+    //Programmierer B -> 3 Tage (Tag 1 bis Tag 3) 
+    public class CarService : ICarService
+    {
+        public void Repair(ICar car)
+        {
+           //reapiere Auto 
+        }
+    }
+
+    #endregion
+
+    #region TestLib.dll
+
+    public class MockCar : ICar
+    {
+        public string Brand { get; set; } = "BMW";
+        public string Model { get; set; } = "7er";
+        public int ConstructionYear { get; set; } = 2022;
+    }
+    #endregion
+
+    #endregion
 }
---
+++ CSharp_Grundkurs/SOLID_005_InterfaceSegregationPrincip/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLID_005_InterfaceSegregationPrincip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IRepository<Employee> employeeRepository = new UnflexibleRepository<Employee>();
+            //employeeRepository.Insert();
+            //employeeRepository.Delete();
+            //employeeRepository.Update();
+            //employeeRepository.GetAll();
+            //employeeRepository.GetById(1);
+
+            IReadCommand<Employee> readCommand = new BetterRepository<Employee>();
+
+            //readCommand.GetAll();
+            //readCommand.GetById(123);
+        }
+    }
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+
+
+    #region BadCode
+    public interface IRepository<T>
+    {
+        T GetById(int id);
+        List<T> GetAll();
+
+        void Update(T entity);
+        void Insert(T entity);
+        void Delete(T entity);
+    }
+
+    public class UnflexibleRepository<T> : IRepository<T>
+    {
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    #endregion
+
+    #region Better Code
+
+    public interface IInsertCommand<T>
+    {
+        void Insert(T item);
+    }
+
+    public interface IUpdateCommand<T>
+    {
+        void Update(T item);
+    }
+
+    public interface IDeleteCommand<T>
+    {
+        void Delete(T item);
+    }
+
+    public interface IReadCommand<T>
+    {
+        T GetById(int id);
+        List<T> GetAll();
+    }
+
+    public class BetterRepository<T> : IInsertCommand<T>, IUpdateCommand<T>, IDeleteCommand<T>, IReadCommand<T>
+    {
+        public void Delete(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
